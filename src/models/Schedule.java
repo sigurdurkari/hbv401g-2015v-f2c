3bc218cd404075366f0126740dbb64970d1e2c38
Sigurður Kári Árnason <ska6@hi.is>
2015-02-25 16:12:35 +0000

    Added the rest of the model classes

--- src/models/Schedule.java
+++ src/models/Schedule.java
@@ -10,7 +10,66 @@ public class Schedule {
 
 	public Schedule() {}
 	
-	public Schedule(List<Team> teams) {
-		
+	public void makeSchedule() {
+		matches = new ArrayList<>();
+		Team ghost = new Team("ghost");
+		if(teams.size()%2==1) {
+			teams.add(ghost);
+		}
+		for(int week=0; week<teams.size()-1; week++) {
+			for(int i=0; i<teams.size()/2; i++) {
+				int home = (week + i) % (teams.size() - 1);
+	            int away = (teams.size() - 1 - i + week) % (teams.size() - 1);
+				matches.add(new ScheduledMatch(teams.get(home),teams.get(away),new Integer(week+1)));
+			}
+		}
+		teams.remove(ghost);
+		for(ScheduledMatch match : matches) {
+			if(match.getHome().getName().equals(ghost.getName()) || match.getAway().getName().equals(ghost.getName())) {
+				matches.remove(match);
+			}
+		}
+	}
+
+	public List<Team> getTeams() {
+		return teams;
+	}
+
+	public void setTeams(List<Team> teams) {
+		this.teams = teams;
+	}
+	
+	public void addTeam(Team team) {
+		this.teams.add(team);
+	}
+	
+	public void removeTeam(Team team) {
+		this.teams.remove(team);
+	}
+
+	public List<ScheduledMatch> getMatches() {
+		return matches;
+	}
+
+	public void setMatches(List<ScheduledMatch> matches) {
+		this.matches = matches;
+	}
+	
+	public void addMatch(ScheduledMatch match) {
+		this.matches.add(match);
+	}
+	
+	public void removeMatch(ScheduledMatch match) {
+		this.matches.remove(match);
+	}
+	
+	public List<ScheduledMatch> getMatchesByWeek(int week) {
+		List<ScheduledMatch> weekMatches = new ArrayList<>();
+		for(ScheduledMatch match : this.matches) {
+			if(week == match.week.intValue()) {
+				weekMatches.add(match);
+			}
+		}
+		return weekMatches;
 	}
 }
--- src/models/ScheduledMatch.java
+++ src/models/ScheduledMatch.java
@@ -1,5 +1,40 @@
 package models;
 
 public class ScheduledMatch {
+	
+	public Team home, away;
+	public Integer week;
+	
+	public ScheduledMatch() {}
+	
+	public ScheduledMatch(Team home, Team away, Integer week) {
+		this.home = home;
+		this.away = away;
+		this.week = week;
+	}
 
+	public Team getHome() {
+		return home;
+	}
+
+	public void setHome(Team home) {
+		this.home = home;
+	}
+
+	public Team getAway() {
+		return away;
+	}
+
+	public void setAway(Team away) {
+		this.away = away;
+	}
+
+	public Integer getWeek() {
+		return week;
+	}
+
+	public void setWeek(Integer week) {
+		this.week = week;
+	}
+	
 }
