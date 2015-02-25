5b45530817d01273f713e56dc5b9df4fb651b334
Sigurður Kári Árnason <ska6@hi.is>
2015-02-25 15:31:34 +0000

    A few more model classes

---
+++ src/models/Schedule.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Schedule {
+	
+	List<Team> teams = new ArrayList<>();
+	List<ScheduledMatch> matches = new ArrayList<>();
+
+	public Schedule() {}
+	
+	public Schedule(List<Team> teams) {
+		
+	}
+}
---
+++ src/models/Team.java
@@ -0,0 +1,41 @@
+package models;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Team {
+	
+	private String name;
+	private List<Player> players = new ArrayList<>();
+	
+	public Team() {}
+	
+	public Team(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Player> getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(List<Player> players) {
+		this.players = players;
+	}
+	
+	public void addPlayer(Player player) {
+		players.add(player);
+	}
+	
+	public void removePlayer(Player player) {
+		players.remove(player);
+	}
+
+}
