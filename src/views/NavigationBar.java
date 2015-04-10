3d8773bf554c38493b3bf51613687b4391d73a4b
gudjonragnar <gudjon.8@gmail.com>
2015-04-10 15:59:24 +0000

    StandingsView added

--- src/views/NavigationBar.java
+++ src/views/NavigationBar.java
@@ -3,6 +3,8 @@ package views;
 import javax.swing.*;
 import java.awt.*;
 import views.*;
+import tests.*;
+import models.*;
 
 
 public class NavigationBar extends JTabbedPane {
@@ -10,20 +12,21 @@ public class NavigationBar extends JTabbedPane {
 	private static final long serialVersionUID = 1L;
 	
 
-	public NavigationBar() {
+	public NavigationBar(Game game) {
 		JPanel MyRosterView = new MakeRosterPanel();
 		addTab("My Roster",MyRosterView);
-		JPanel MyRosterView2 = new MakeRosterPanel();
-		addTab("My Roster no 2",MyRosterView2);
+		JPanel StandingsView = new StandingsView(game);
+		addTab("Standings",StandingsView);
 	}
 	
 	public static void main(String[] args){
+		Game game = BasicEntities.generateGame();
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
 		frame.setSize(900,700);
-		JTabbedPane panel = new NavigationBar();
+		JTabbedPane panel = new NavigationBar(game);
 		frame.add(panel);
 		frame.setVisible(true);
 		frame.setContentPane(panel);
